118685123bbdcb32fa64ad08da6ec563acdc3032
jcdcm <jcdcm20@gmail.com>
2023-07-05 11:47:19 -0300

    Get zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,5 +5,6 @@ puts 'Event Manager Initialized!'
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 contents.each do |row|
     name = row[:first_name]
-    puts name
+    zipcode = row[:zipcode]
+    puts "#{name} #{zipcode}"
 end
