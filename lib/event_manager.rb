e3589e8bb0367e13e2eeb036a782ac4b74886bf8
jcdcm <jcdcm20@gmail.com>
2023-07-05 14:33:39 -0300

    Clear presentation

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -23,9 +23,13 @@ contents.each do |row|
         )
 
         legislators = legislators.officials
+
+        legislator_names = legislators.map(&:name)
+
+        legislators_string = legislator_names.join(", ")
     rescue
         'You can find your reprensatatives by visiting www.commoncause.org/take-action/find-elected-officials'
     end
-    
-    puts "#{name} #{zipcode} #{legislators}"
+
+    puts "#{name} #{zipcode} #{legislators_string}"
 end
