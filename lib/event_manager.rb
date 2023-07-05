aaf9e80c49c0f48d615068aa4d2f8ce1a24ab208
jcdcm <jcdcm20@gmail.com>
2023-07-05 14:41:44 -0300

    Encapsulate legislators into method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,35 +1,43 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
-    zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts 'Event Manager Initialized!'
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
 contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
+  name = row[:first_name]
 
-    begin
-        legislators = civic_info.representative_info_by_address(
-            address: zipcode,
-            levels: 'country',
-            roles: ['legislatorUpperBody', 'legislatorLowerBody']
-        )
+  zipcode = clean_zipcode(row[:zipcode])
 
-        legislators = legislators.officials
+  legislators = legislators_by_zipcode(zipcode)
 
-        legislator_names = legislators.map(&:name)
-
-        legislators_string = legislator_names.join(", ")
-    rescue
-        'You can find your reprensatatives by visiting www.commoncause.org/take-action/find-elected-officials'
-    end
-
-    puts "#{name} #{zipcode} #{legislators_string}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
